ba25ba122180a61173d63cee5db5b97899720603
EPLCLooser <lucas@norrflod.se>
2026-03-04 16:08:49 +0100

    Kommit längre på login funktionen men stötte på några problem

--- app.rb
+++ app.rb
@@ -7,6 +7,14 @@ require_relative './model.rb'
 
 enable :sessions
 
+@all_routes = ['/', '/events', '/events/:id', '/events/new', '/events/:id/edit']
+
+before ('/events/*') do
+  if session[:logged_in] == nil 
+    redirect to ('/registrer')
+  end
+end
+
 get('/') do
   redirect('/events')
 end
@@ -27,6 +35,10 @@ get('/events/:id/edit') do
   slim(:edit)
 end
 
+get('/registrer') do 
+  slim(:registrer)
+end
+
 post('/events') do
   redirect('/events')
 end
@@ -37,4 +49,38 @@ end
 
 post('/events/:id/delete') do
   redirect('/events')
+end
+
+post('/events/login') do
+  user = params["user"]
+  pwd = params["pwd"]
+  pwd_confirm = params["pwd_confirm"]
+  if registrer(user, pwd, pwd_confirm)
+    session[:logged_in] = true
+    redirect('/events')
+  else
+    redirect('/error')
+  end 
+end
+
+post('/events/user') do
+  user = params["user"]
+  pwd = params["pwd"]
+  pwd_confirm = params["pwd_confirm"]
+
+  db = SQLite3::Database.new("db/todos.db")
+  result = db.execute("SELECT id FROM users WHERE username=?", user)
+
+  if result.empty?
+    if pwd==pwd_confirm
+      pwd_digest=BCrypt::Password.create(pwd)
+      db.execute("INSERT INTO users(username, password) VALUES(?,?)", [user, pwd_digest])
+      session[:logged_in] = true
+      redirect('/')
+    else
+      redirect('/error') 
+    end
+  else
+    redirect('/login_page')
+  end
 end
--- model.rb
+++ model.rb
@@ -1,14 +1,10 @@
-post('/events/login') do
-  user = params["user"]
-  pwd = params["pwd"]
-  pwd_confirm = params["pwd_confirm"]
-
+def registrer(user, pwd, pwd_confirm)
   db = SQLite3::Database.new("db/todos.db")
   db.results_as_hash = true
   result = db.execute("SELECT id, password FROM users WHERE username=?", user)
   
   if result.empty?
-    redirect('/error') #wrong password/username
+    return false #wrong password/username
   end
   user_id = result.first["id"]
   pwd_digest = result.first["password"]
@@ -16,30 +12,12 @@ post('/events/login') do
   if BCrypt::Password.new(pwd_digest) == pwd
     session[:user_id] = user_id
     session[:logged_in] = true
-    redirect('/')
+    return true
   else
-    redirect("/error")
+    return false
   end
 end
 
-post('/events/user') do
-  user = params["user"]
-  pwd = params["pwd"]
-  pwd_confirm = params["pwd_confirm"]
+def login(user, pwd)
 
-  db = SQLite3::Database.new("db/todos.db")
-  result = db.execute("SELECT id FROM users WHERE username=?", user)
-
-  if result.empty?
-    if pwd==pwd_confirm
-      pwd_digest=BCrypt::Password.create(pwd)
-      db.execute("INSERT INTO users(username, password) VALUES(?,?)", [user, pwd_digest])
-      session[:logged_in] = true
-      redirect('/')
-    else
-      redirect('/error') 
-    end
-  else
-    redirect('/login_page')
-  end
 end
