c679660b975853eea9b9bc443cb21cfae8a62752
EPLCLooser <lucas@norrflod.se>
2026-03-26 15:17:52 +0100

    påsklov inläming

--- app.rb
+++ app.rb
@@ -4,20 +4,34 @@ require 'sqlite3'
 require 'sinatra/reloader'
 require 'bcrypt'
 require_relative './model.rb'
+
+include Model
+
 enable :sessions
 
 @all_routes = ['/', '/events', '/events/:id', '/events_new', '/events/:id/edit']
 
+# A before function that checks if a user is logged in and if not they are redirected to register page
+# 
 before(@all_routes) do
   if session[:logged_in] != true || session[:ban]
     redirect('/register')
   end
 end
 
+# GET /
+# Redirects to the events page
+#
+# @return [Response] redirects to /events
 get('/') do
   redirect('/events')
 end
 
+# GET /events
+# Displays all events that the user has permission to see
+#
+# @param [Hash] params query parameters, including optional event_type
+# @return [String] rendered slim template for index
 get('/events') do 
   type_search = params[:event_type]
   if type_search == ""
@@ -31,28 +45,56 @@ get('/events') do
   slim(:index)
 end
 
+# GET /error
+# Displays the error message from the session
+#
+# @return [String] rendered slim template for error
 get('/error') do
   @error = session[:error]
   slim(:error)
 end
 
+# GET /events/:id
+# Displays details of a specific event
+#
+# @param [String] id the event ID from the URL
+# @return [String] rendered slim template for event show
 get('/events/:id') do 
   @event, @owner = get_event(params[:id])
   slim(:"./events/show")
 end
 
+# GET /events_new
+# Displays the page for creating a new event
+#
+# @return [String] rendered slim template for new event
 get('/events_new') do
   slim(:"./events/new")
 end
 
+# GET /events/:id/edit
+# Displays the page for editing an event
+#
+# @param [String] id the event ID from the URL
+# @return [String] rendered slim template for event edit
 get('/events/:id/edit') do 
   slim(:"./events/edit")
 end
 
+# GET /register
+# Displays the register and login page
+#
+# @return [String] rendered slim template for user registration
 get('/register') do 
   slim(:"./user/new")
 end
 
+
+# POST /events/create
+# Creates a new event with the provided parameters
+#
+# @param [Hash] params form parameters including name, place, info, date, time, event_type
+# @return [Response] redirects to /events on success, /error on failure
 post('/events/create') do
   name = params[:name]
   place = params[:place]
@@ -67,6 +109,12 @@ post('/events/create') do
   end
 end
 
+# POST /events/:id/update
+# Updates an existing event with new details
+#
+# @param [String] id the event ID from the URL
+# @param [Hash] params form parameters including name, place, info, date, time, event_type
+# @return [Response] redirects to /events on success, /error on failure
 post('/events/:id/update') do
   id = params[:id]
   name = params[:name]
@@ -83,6 +131,11 @@ post('/events/:id/update') do
   redirect('/events')
 end
 
+# POST /events/:id/delete
+# Deletes the specified event
+#
+# @param [String] id the event ID from the URL
+# @return [Response] redirects to /events on success, /error on failure
 post('/events/:id/delete') do
   event_id = params[:id]
   if delete_event(event_id)
@@ -92,6 +145,11 @@ post('/events/:id/delete') do
   end
 end
 
+# POST /login
+# Logs in a user with the provided credentials
+#
+# @param [Hash] params form parameters including user and pwd
+# @return [Response] redirects to /events on success, /error on failure
 post('/login') do
   user = params["user"]
   pwd = params["pwd"]
@@ -103,6 +161,11 @@ post('/login') do
   end 
 end
 
+# POST /user
+# Registers a new user
+#
+# @param [Hash] params form parameters including user, pwd, pwd_confirm
+# @return [Response] redirects to /events on success, /error on failure
 post('/user') do
   user = params["user"]
   pwd = params["pwd"]
@@ -115,6 +178,11 @@ post('/user') do
   end
 end
 
+# POST /join
+# Joins an event using the provided code
+#
+# @param [Hash] params form parameters including code
+# @return [Response] redirects to /events on success, /error on failure
 post('/join') do
   code = params[:code]
   if join(code)
@@ -124,11 +192,20 @@ post('/join') do
   end
 end
 
+# POST /log_out
+# Logs out the current user
+#
+# @return [Response] redirects to /events
 post("/log_out") do
   session[:logged_in] = false
   redirect("/events")
 end
 
+# POST /user/:id/delete
+# Deletes the specified user (admin only)
+#
+# @param [String] id the user ID from the URL
+# @return [Response] redirects to /events on success, /error on failure
 post("/user/:id/delete") do
   id = params[:id]
   if delete_user(id)
--- model.rb
+++ model.rb
@@ -1,192 +1,253 @@
-def register(user, pwd, pwd_confirm)
-  db = SQLite3::Database.new("db/databas.db")
-  db.results_as_hash = true
-  result = db.execute("SELECT id FROM users WHERE user=?", user)
-  if result.empty? 
-    if pwd==pwd_confirm
-      pwd_digest=BCrypt::Password.create(pwd)
-      db.execute("INSERT INTO users(user, password, admin) VALUES(?,?,?)", [user, pwd_digest,0])
-      result = db.execute("SELECT id FROM users WHERE user=?", user)
-      session[:user_id] = result.first["id"]
-      return true
-    else
-      session[:error] = "Password and password confirm is not matching"
-      return false 
-    end
-  else
-    session[:error] = "There already exist a user with that name"
-    return false
-  end
-end
-
-def login(user, pwd)
-  db = SQLite3::Database.new("db/databas.db")
-  db.results_as_hash = true
-  result = db.execute("SELECT id, password FROM users WHERE user=?", user)
-  
-  if session[:time] == nil || Time.now.to_i - session[:time] > 10
-    session[:time] = Time.now.to_i
-    session[:tries] = 0
-  elsif session[:tries] >= 10
-    session[:ban] = true
-  end
-  session[:tries] += 1
-
-  if result.empty?
-    return false #wrong username
-  end
-  user_id = result.first["id"]
-  pwd_digest = result.first["password"]
-
-  if BCrypt::Password.new(pwd_digest) == pwd && session[:ban] != true
-    session[:user_id] = user_id
-    return true
-  else
-    return false
-  end
-end
-
-def add_event(name, place, info, date, time, event_type_id)
-  db = SQLite3::Database.new("db/databas.db")
-  db.results_as_hash = true
-  result = db.execute("SELECT id FROM events WHERE name=?", name)
-  code = rand(10000..100000)
-  code_result = db.execute("SELECT id FROM events WHERE code=?", code)
-  while !code_result.empty?
-    code += 1
-    code_result = db.execute("SELECT id FROM events WHERE code=?", code)
-  end
-  if result.empty?
-    db.execute("INSERT INTO events(code, name, place, info, date, time, event_type_id) VALUES(?,?,?,?,?,?,?)", [code, name, place, info, date, time, event_type_id])
-    result = db.execute("SELECT id FROM events WHERE name=?", name)
-    event_id = result.first["id"]
-    db.execute("INSERT INTO users_events(user_id, event_id, user_owner) VALUES(?,?,?)", [session[:user_id], event_id, 1])
-    return true
-  else
-    session[:error] = "There is already an event with the name of #{name}"
-    return false
-  end
-end
-
-def join(code)
-  db = SQLite3::Database.new("db/databas.db")
-  db.results_as_hash = true
-  result = db.execute("SELECT events.id, users_events.user_id FROM (users_events INNER JOIN events ON users_events.event_id = events.id) WHERE code=?", code)
-  already_joined = false
-  for dict in result
-    if dict["user_id"] == session[:user_id]
-      already_joined = true
-    end
-  end
-  if result.empty? || already_joined
-    session[:error] = "There is no event with the code: #{code}"
-    return false
-  else
-    db.execute("INSERT INTO users_events(user_id, event_id, user_owner) VALUES(?,?,?)",[session[:user_id], result.first["id"], 0])
-    return true
-  end
-end
-
-def show_events(type_search)
-  db = SQLite3::Database.new("db/databas.db")
-  db.results_as_hash = true
-  if is_admin?()
-    admin=1
-  end
-  owned_events = db.execute("SELECT events.name, events.id, events.code FROM ((users_events INNER JOIN events ON users_events.event_id = events.id) INNER JOIN event_types ON events.event_type_id = event_types.id) WHERE (users_events.user_id=? AND users_events.user_owner=? AND (event_types.id=? OR ? IS NULL)) OR (1=?  AND users_events.user_owner=?)", [session[:user_id], 1, type_search, type_search, admin, 1])
-  joined_events = db.execute("SELECT events.name, events.id FROM ((users_events INNER JOIN events ON users_events.event_id = events.id) INNER JOIN event_types ON events.event_type_id = event_types.id) WHERE users_events.user_id=? AND users_events.user_owner=? AND (event_types.id=? OR ? IS NULL)", [session[:user_id], 0, type_search, type_search])
-  return [owned_events, joined_events]
-end
-
-#Returns a dictionary of the event's information given its id
-def get_event(event_id)
-  db = SQLite3::Database.new("db/databas.db")
-  db.results_as_hash = true
-  if is_admin?()
-    result = db.execute("SELECT * FROM users_events WHERE event_id=?", event_id)
-  else
-    result = db.execute("SELECT * FROM users_events WHERE user_id=? AND event_id=?", [session[:user_id], event_id])
-  end
-  if result.empty?
-    session[:error] = "You are not an user in this event"
-    redirect("/error") #User is not in event
-  elsif is_admin?()
-    event = db.execute("SELECT event_types.name AS event_type_name, * FROM ((event_types INNER JOIN events ON events.event_type_id = event_types.id) INNER JOIN users_events ON events.id = users_events.event_id) WHERE events.id=?", [event_id,1]).first
-  else
-    event = db.execute("SELECT event_types.name AS event_type_name, * FROM ((event_types INNER JOIN events ON events.event_type_id = event_types.id) INNER JOIN users_events ON events.id = users_events.event_id) WHERE events.id=? AND users_events.user_id=?", [event_id, session[:user_id]]).first
-  end
-  return event, event["user_owner"]
-end
-
-def edit_event(name, place, info, date, time, event_type_id, id)
-  db = SQLite3::Database.new("db/databas.db")
-  db.results_as_hash = true
-  if authorize(id) || is_admin?()
-    db.execute("UPDATE events SET name=?, place=?, info=?, date=?, time=?, event_type_id=? WHERE id=? ", [name, place, info, date, time, event_type_id, id])
-    result = db.execute("SELECT id FROM events WHERE name=?", name)
-    event_id = result.first["id"]
-    return true
-  else
-    session[:error] = "You are not the owner of this event"
-    return false
-  end
-end
-
-def delete_event(event_id)
-  db = SQLite3::Database.new("db/databas.db")
-  db.results_as_hash = true
-  if authorize(event_id) || is_admin?()
-    db.execute("DELETE FROM events WHERE id=?", event_id)
-    db.execute("DELETE FROM users_events WHERE event_id=?", event_id)
-    return true
-  else
-    session[:error] = "You do not have permission to delete this event"
-    return false #Do not have permission to delete
-  end
-end
-
-def authorize(event_id)
-  db = SQLite3::Database.new("db/databas.db")
-  db.results_as_hash = true
-  result = db.execute("SELECT user_id FROM users_events WHERE user_owner=1 AND event_id=?", event_id)
-  if !result.empty? && result.first["user_id"] == session[:user_id]
-    return true
-  else
-    return false
-  end
-end
-
-def get_users() #get users from users table and display them for admin and make them deletable.
-  db = SQLite3::Database.new("db/databas.db")
-  db.results_as_hash = true
-  users = db.execute("SELECT id, user, admin FROM users WHERE admin=?", 0)
-  return users
-end
-
-def is_admin?()
-  db = SQLite3::Database.new("db/databas.db")
-  db.results_as_hash = true
-  admin = db.execute("SELECT admin FROM users WHERE id=?", session[:user_id]).first["admin"]
-  if admin == 1
-    return true
-  else
-    return false
-  end
-end
-
-def delete_user(id)
-  if is_admin?()
+module Model
+  # Registers a new user in the database
+  #
+  # @param user [String] the username for the new user
+  # @param pwd [String] the password for the new user
+  # @param pwd_confirm [String] the password confirmation
+  # @return [Boolean] true if registration is successful, false otherwise
+  def register(user, pwd, pwd_confirm)
     db = SQLite3::Database.new("db/databas.db")
     db.results_as_hash = true
-    event_ids = db.execute("SELECT event_id FROM users_events WHERE user_id=?", id)
-    db.execute("DELETE FROM events WHERE id IN (SELECT event_id FROM users_events WHERE user_id = ?)", id)
-    db.execute("DELETE FROM users WHERE id=?", id)
-    event_ids.each do |event_id|
-      db.execute("DELETE FROM users_events WHERE event_id=?", event_id["event_id"])
+    result = db.execute("SELECT id FROM users WHERE user=?", user)
+    if result.empty? 
+      if pwd==pwd_confirm
+        pwd_digest=BCrypt::Password.create(pwd)
+        db.execute("INSERT INTO users(user, password, admin) VALUES(?,?,?)", [user, pwd_digest,0])
+        result = db.execute("SELECT id FROM users WHERE user=?", user)
+        session[:user_id] = result.first["id"]
+        return true
+      else
+        session[:error] = "Password and password confirm is not matching"
+        return false 
+      end
+    else
+      session[:error] = "There already exist a user with that name"
+      return false
     end
-    return true
-  else
-    session[:error] = "You do not have permission to delete another user"
-    return false
+  end
+
+  # Logs in a user with the provided credentials
+  #
+  # @param user [String] the username
+  # @param pwd [String] the password
+  # @return [Boolean] true if login is successful, false otherwise
+  def login(user, pwd)
+    db = SQLite3::Database.new("db/databas.db")
+    db.results_as_hash = true
+    result = db.execute("SELECT id, password FROM users WHERE user=?", user)
+    
+    if session[:time] == nil || Time.now.to_i - session[:time] > 10
+      session[:time] = Time.now.to_i
+      session[:tries] = 0
+    elsif session[:tries] >= 10
+      session[:ban] = true
+    end
+    session[:tries] += 1
+
+    if result.empty?
+      return false #wrong username
+    end
+    user_id = result.first["id"]
+    pwd_digest = result.first["password"]
+
+    if BCrypt::Password.new(pwd_digest) == pwd && session[:ban] != true
+      session[:user_id] = user_id
+      return true
+    else
+      return false
+    end
+  end
+
+  # Adds a new event to the database
+  #
+  # @param name [String] the name of the event
+  # @param place [String] the location of the event
+  # @param info [String] additional information about the event
+  # @param date [String] the date of the event
+  # @param time [String] the time of the event
+  # @param event_type_id [Integer] the ID of the event type
+  # @return [Boolean] true if the event is added successfully, false otherwise
+  def add_event(name, place, info, date, time, event_type_id)
+    db = SQLite3::Database.new("db/databas.db")
+    db.results_as_hash = true
+    result = db.execute("SELECT id FROM events WHERE name=?", name)
+    code = rand(10000..100000)
+    code_result = db.execute("SELECT id FROM events WHERE code=?", code)
+    while !code_result.empty?
+      code += 1
+      code_result = db.execute("SELECT id FROM events WHERE code=?", code)
+    end
+    if result.empty?
+      db.execute("INSERT INTO events(code, name, place, info, date, time, event_type_id) VALUES(?,?,?,?,?,?,?)", [code, name, place, info, date, time, event_type_id])
+      result = db.execute("SELECT id FROM events WHERE name=?", name)
+      event_id = result.first["id"]
+      db.execute("INSERT INTO users_events(user_id, event_id, user_owner) VALUES(?,?,?)", [session[:user_id], event_id, 1])
+      return true
+    else
+      session[:error] = "There is already an event with the name of #{name}"
+      return false
+    end
+  end
+
+  # Allows a user to join an event using a code
+  #
+  # @param code [Integer] the event code to join
+  # @return [Boolean] true if joining is successful, false otherwise
+  def join(code)
+    db = SQLite3::Database.new("db/databas.db")
+    db.results_as_hash = true
+    result = db.execute("SELECT events.id, users_events.user_id FROM (users_events INNER JOIN events ON users_events.event_id = events.id) WHERE code=?", code)
+    already_joined = false
+    for dict in result
+      if dict["user_id"] == session[:user_id]
+        already_joined = true
+      end
+    end
+    if result.empty? || already_joined
+      session[:error] = "There is no event with the code: #{code}"
+      return false
+    else
+      db.execute("INSERT INTO users_events(user_id, event_id, user_owner) VALUES(?,?,?)",[session[:user_id], result.first["id"], 0])
+      return true
+    end
+  end
+
+  # Retrieves events owned and joined by the current user, optionally filtered by type
+  #
+  # @param type_search [Integer, nil] the event type ID to filter by, or nil for all
+  # @return [Array<Array<Hash>>] an array containing owned events and joined events
+  def show_events(type_search)
+    db = SQLite3::Database.new("db/databas.db")
+    db.results_as_hash = true
+    if is_admin?()
+      admin=1
+    end
+    owned_events = db.execute("SELECT events.name, events.id, events.code FROM ((users_events INNER JOIN events ON users_events.event_id = events.id) INNER JOIN event_types ON events.event_type_id = event_types.id) WHERE (users_events.user_id=? AND users_events.user_owner=? AND (event_types.id=? OR ? IS NULL)) OR (1=?  AND users_events.user_owner=?)", [session[:user_id], 1, type_search, type_search, admin, 1])
+    joined_events = db.execute("SELECT events.name, events.id FROM ((users_events INNER JOIN events ON users_events.event_id = events.id) INNER JOIN event_types ON events.event_type_id = event_types.id) WHERE users_events.user_id=? AND users_events.user_owner=? AND (event_types.id=? OR ? IS NULL)", [session[:user_id], 0, type_search, type_search])
+    return [owned_events, joined_events]
+  end
+
+  # Retrieves information about a specific event
+  #
+  # @param event_id [Integer] the ID of the event
+  # @return [Array<Hash, Integer>] an array containing the event data and ownership status
+  def get_event(event_id)
+    db = SQLite3::Database.new("db/databas.db")
+    db.results_as_hash = true
+    if is_admin?()
+      result = db.execute("SELECT * FROM users_events WHERE event_id=?", event_id)
+    else
+      result = db.execute("SELECT * FROM users_events WHERE user_id=? AND event_id=?", [session[:user_id], event_id])
+    end
+    if result.empty?
+      session[:error] = "You are not an user in this event"
+      redirect("/error") #User is not in event
+    elsif is_admin?()
+      event = db.execute("SELECT event_types.name AS event_type_name, * FROM ((event_types INNER JOIN events ON events.event_type_id = event_types.id) INNER JOIN users_events ON events.id = users_events.event_id) WHERE events.id=?", [event_id,1]).first
+    else
+      event = db.execute("SELECT event_types.name AS event_type_name, * FROM ((event_types INNER JOIN events ON events.event_type_id = event_types.id) INNER JOIN users_events ON events.id = users_events.event_id) WHERE events.id=? AND users_events.user_id=?", [event_id, session[:user_id]]).first
+    end
+    return event, event["user_owner"]
+  end
+
+  # Edits an existing event's details
+  #
+  # @param name [String] the new name of the event
+  # @param place [String] the new location
+  # @param info [String] the new information
+  # @param date [String] the new date
+  # @param time [String] the new time
+  # @param event_type_id [Integer] the new event type ID
+  # @param id [Integer] the ID of the event to edit
+  # @return [Boolean] true if editing is successful, false otherwise
+  def edit_event(name, place, info, date, time, event_type_id, id)
+    db = SQLite3::Database.new("db/databas.db")
+    db.results_as_hash = true
+    if authorize(id) || is_admin?()
+      db.execute("UPDATE events SET name=?, place=?, info=?, date=?, time=?, event_type_id=? WHERE id=? ", [name, place, info, date, time, event_type_id, id])
+      result = db.execute("SELECT id FROM events WHERE name=?", name)
+      event_id = result.first["id"]
+      return true
+    else
+      session[:error] = "You are not the owner of this event"
+      return false
+    end
+  end
+
+  # Deletes an event from the database
+  #
+  # @param event_id [Integer] the ID of the event to delete
+  # @return [Boolean] true if deletion is successful, false otherwise
+  def delete_event(event_id)
+    db = SQLite3::Database.new("db/databas.db")
+    db.results_as_hash = true
+    if authorize(event_id) || is_admin?()
+      db.execute("DELETE FROM events WHERE id=?", event_id)
+      db.execute("DELETE FROM users_events WHERE event_id=?", event_id)
+      return true
+    else
+      session[:error] = "You do not have permission to delete this event"
+      return false #Do not have permission to delete
+    end
+  end
+
+  # Checks if the current user is the owner of the event
+  #
+  # @param event_id [Integer] the ID of the event
+  # @return [Boolean] true if the user is the owner, false otherwise
+  def authorize(event_id)
+    db = SQLite3::Database.new("db/databas.db")
+    db.results_as_hash = true
+    result = db.execute("SELECT user_id FROM users_events WHERE user_owner=1 AND event_id=?", event_id)
+    if !result.empty? && result.first["user_id"] == session[:user_id]
+      return true
+    else
+      return false
+    end
+  end
+
+  # Retrieves all non-admin users
+  #
+  # @return [Array<Hash>] an array of user hashes with id, user, and admin status
+  def get_users() #get users from users table and display them for admin and make them deletable.
+    db = SQLite3::Database.new("db/databas.db")
+    db.results_as_hash = true
+    users = db.execute("SELECT id, user, admin FROM users WHERE admin=?", 0)
+    return users
+  end
+
+  # Checks if the current user is an admin
+  #
+  # @return [Boolean] true if the user is an admin, false otherwise
+  def is_admin?()
+    db = SQLite3::Database.new("db/databas.db")
+    db.results_as_hash = true
+    admin = db.execute("SELECT admin FROM users WHERE id=?", session[:user_id]).first["admin"]
+    if admin == 1
+      return true
+    else
+      return false
+    end
+  end
+
+  # Deletes a user and all their associated events
+  #
+  # @param id [Integer] the ID of the user to delete
+  # @return [Boolean] true if deletion is successful, false otherwise
+  def delete_user(id)
+    if is_admin?()
+      db = SQLite3::Database.new("db/databas.db")
+      db.results_as_hash = true
+      event_ids = db.execute("SELECT event_id FROM users_events WHERE user_id=?", id)
+      db.execute("DELETE FROM events WHERE id IN (SELECT event_id FROM users_events WHERE user_id = ?)", id)
+      db.execute("DELETE FROM users WHERE id=?", id)
+      event_ids.each do |event_id|
+        db.execute("DELETE FROM users_events WHERE event_id=?", event_id["event_id"])
+      end
+      return true
+    else
+      session[:error] = "You do not have permission to delete another user"
+      return false
+    end
   end
 end
